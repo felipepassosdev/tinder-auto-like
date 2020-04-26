898ebed1e63d28255d065a275b2e9ae5cb8c3e36
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-04-26 10:14:29 -0300

    att

--- content.js
+++ content.js
@@ -1,27 +1,5 @@
-const criarBotao = () => {
-    var row = document.querySelector('.recsToolbar')
-    var automatico = document.createElement('div')
-
-    automatico.setAttribute('class', 'automatico')
-    automatico.innerText = "MEGALIKE"
-
-    row.appendChild(automatico)
-}
-
-
-window.onload = function () {
-    criarBotao()
-}
-
 setInterval(() => {
-    const botao = document.querySelectorAll('.button')[2];
-    const botao2 = document.querySelectorAll('.button');
-    const x = document.querySelector('.automatico')
+    var botao = document.querySelectorAll('.button')[2]
 
-    botao2.forEach((item) => {
-        x.addEventListener('click', () => {
-            botao.click()
-        })
-    })
-    
-}, 1000)
+    botao.click()
+},1000)
