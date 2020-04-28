8848d6834d88ce3f67ef2e67bc345ce4bc8153af
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-04-28 19:41:06 -0300

    Melhoria no sistema de like automatico

--- content.js
+++ content.js
@@ -1,5 +1,25 @@
-setInterval(() => {
-    var botao = document.querySelectorAll('.button')[2]
+var interval = setInterval(() => {
+  var modal = document.querySelectorAll(".modal-zoom-in");
+  var match = document.querySelectorAll(".itsAMatch");
+  console.log("var modal", modal);
+  console.log("var match", match);
 
-    botao.click()
-},1000)
+  if (match.length > 0) {
+    console.log("match");
+    var botao = document.querySelectorAll(".active");
+    if (botao.length > 1) {
+      botao = botao[1];
+      botao.click();
+    }
+  } else if (modal.length === 0) {
+    console.log("like");
+    var botao = document.querySelectorAll(".button");
+    if (botao.length > 2) {
+      botao = botao[2];
+      botao.click();
+    }
+  } else {
+    console.log("end");
+    clearInterval(interval);
+  }
+}, 1500);
